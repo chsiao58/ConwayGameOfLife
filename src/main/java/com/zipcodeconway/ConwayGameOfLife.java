41526743e5c0ef7aee77dfe560246593d971c1ad
Joanna Hsiao <50811279+chsiao58@users.noreply.github.com>
2020-02-18 20:44:29 -0500

    isAlive logic update

--- src/main/java/com/zipcodeconway/ConwayGameOfLife.java
+++ src/main/java/com/zipcodeconway/ConwayGameOfLife.java
@@ -1,11 +1,18 @@
 package com.zipcodeconway;
 
 public class ConwayGameOfLife {
+    Integer dimension;
+    int[][] matrix;
 
     public ConwayGameOfLife(Integer dimension) {
+        this.dimension = dimension;
+        matrix = createRandomStart(dimension);
+
      }
 
     public ConwayGameOfLife(Integer dimension, int[][] startmatrix) {
+        this.dimension = dimension;
+        this.matrix = startmatrix;
     }
 
     public static void main(String[] args) {
@@ -38,6 +45,32 @@ public class ConwayGameOfLife {
 		Any dead cell with exactly three live neighbours cells will come to life.
 	*/
     private int isAlive(int row, int col, int[][] world) {
-        return 0;
+        int numOfLiveNeighbor = 0;
+        for (int i = row-1; i <= row+1; i++) {
+            for(int j = col-1; j <= col+1; j++) {
+                if (    !(i == row && j == col) &&
+                         (i >= 0 && i < world[0].length) &&
+                         (j >= 0 && j < world[0].length)) {
+                    if(world[i][j] == 1)
+                        numOfLiveNeighbor++;
+                }
+            }
+        }
+
+        if(world[row][col] == 0) {
+            if (numOfLiveNeighbor == 3)
+                return 1;
+            else
+                return 0;
+        }
+
+        else if(world[row][col] == 1) {
+            if (numOfLiveNeighbor == 2 || numOfLiveNeighbor == 3)
+                return 1;
+            else
+                return 0;
+        }
+
+        throw new IllegalStateException();
     }
 }
